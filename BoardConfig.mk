8b1eabf97829cc9a74818fa3c3e8b499484d6281
pandaxgc <40298249+pandaxgc@users.noreply.github.com>
2024-04-10 23:38:45 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@
 # Inherit from sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/xiaomi/alioth
+DEVICE_PATH := device/xiaomi/psyche
 
 BUILD_BROKEN_DUP_RULES := true
 
@@ -15,11 +15,11 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Display
-TARGET_SCREEN_DENSITY := 420
+TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_psyche
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_psyche
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
